1a80b87ef040c78137e547fc72556af93c73ee80
Kakeshin <kakeshin.engineer@gmail.com>
2023-07-14 00:52:25 +0900

    Initial Commit

---
+++ src/main.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { BrowserRouter } from 'react-router-dom'
+
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+  <BrowserRouter>
+    <h1>Hello World</h1>
+  </BrowserRouter>,
+)
